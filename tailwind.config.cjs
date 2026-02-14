caa2b5fba135a7e9972d2f2c883ec8d806b6f778
Pjetko <pjetko@gmail.com>
2026-02-14 20:46:55 +0100

    fix: remove cdn tailwind and inter font, clean up index.html

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -39,7 +39,7 @@ module.exports = {
         },
       },
       fontFamily: {
-        sans: ['Inter', 'Segoe UI', 'Helvetica', 'Arial', 'sans-serif'],
+        sans: ['system-ui', 'Segoe UI', 'Helvetica', 'Arial', 'sans-serif'],
       },
       spacing: {
         128: '32rem',
